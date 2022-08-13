e3e45073e7fd1421a6f8880f23bd9428970f8728
Yuna <85014628+vipwhy12@users.noreply.github.com>
2022-08-14 02:19:23 +0900

    import requests, BeautifulSoup

---
+++ prac_python/hello.py
@@ -0,0 +1,7 @@
+import requests # requests 라이브러리 설치 필요
+
+r = requests.get('http://openapi.seoul.go.kr:8088/6d4d776b466c656533356a4b4b5872/json/RealtimeCityAir/1/99')
+rjson = r.json()
+
+print(rjson)
+print(rjson['RealtimeCityAir']['row'][0]['NO2']) # 중구의 NO2 값
