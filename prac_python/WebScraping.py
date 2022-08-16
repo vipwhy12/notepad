4a8390e46b84ecdb30edad2c2ba081f2cece9045
Yuna <85014628+vipwhy12@users.noreply.github.com>
2022-08-17 03:17:35 +0900

    Update WebScraping.py

--- prac_python/WebScraping.py
+++ prac_python/WebScraping.py
@@ -1,26 +1,16 @@
+from wsgiref import headers
 import requests
 from bs4 import BeautifulSoup
 
-# 타겟 URL을 읽어서 HTML를 받아오고,
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('https://cu.bgfretail.com/product/view.do?category=event&gdIdx=1',headers=headers)
-for i in range(2):
-    http = f'https://cu.bgfretail.com/product/view.do?category=event&gdIdx={i}'
-    print(http)
-    data = requests.get(http,headers=headers)
-# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
-# soup이라는 변수에 "파싱 용이해진 html"이 담긴 상태가 됨
-# 이제 코딩을 통해 필요한 부분을 추출하면 된다.
-    print(data)
+data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200303',headers=headers)
 
+soup = BeautifulSoup(data.text, 'html.parser')
+naver_movie_ranking = soup.select('#old_content tr')
 
-    soup = BeautifulSoup(data.text, 'html.parser')
-    foods = soup.select('li.prod_list')
+for movie in naver_movie_ranking:
+    rank = movie.select_one('td.ac > img')
 
+    if rank is not None:
+        print(rank['alt'])
 
-##wrap > #contents > .relCon > .prodListWrap  > 
-
-
-
-for food in foods:
-    print(food)
